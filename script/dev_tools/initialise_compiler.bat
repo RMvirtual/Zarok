251bb9731b52826fc42df102b604bcdb84df9830
RMvirtual <rmvirtual@AOL.com>
2022-12-26 11:35:31 +0000

    Comments added to scripting.

--- script/dev_tools/initialise_compiler.bat
+++ script/dev_tools/initialise_compiler.bat
@@ -1,8 +1,11 @@
 @echo off
+
+: Create W: drive path for working git/dev directory.
 SUBST w: %HOMEPATH%\Documents\GitHub\Compiler_Exercises
 SET PATH=w:%path%
 W:
 
+: Start MSVC dev environment for compiler command.
 CALL ^
     "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community"^
 "\VC\Auxiliary\Build\vcvarsall.bat" x64
