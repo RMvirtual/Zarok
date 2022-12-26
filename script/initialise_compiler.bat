bf39728e50fb4c0ce697002c4bc971019df67922
RMvirtual <rmvirtual@AOL.com>
2022-12-26 11:06:49 +0000

    Work on drive lettering.

--- script/initialise_compiler.bat
+++ script/initialise_compiler.bat
@@ -1,3 +1,5 @@
+SUBST w: %HOMEPATH%\Documents\GitHub\Compiler_Exercises
 CALL ^
     "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community"^
 "\VC\Auxiliary\Build\vcvarsall.bat" x64
+SET PATH=w:%path%
