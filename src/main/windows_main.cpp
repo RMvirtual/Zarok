b09e4ec7ac4698825f011bd0cc2af1c8fb341e3f
RMvirtual <rmvirtual@AOL.com>
2023-01-01 12:06:18 +0000

    Code tidy.

--- src/main/windows_main.cpp
+++ src/main/windows_main.cpp
@@ -46,35 +46,29 @@ int CALLBACK WinMain(
 
   // Run the message loop.
   MSG msg = {};
-  while (GetMessage(&msg, NULL, 0, 0) > 0)
-  {
+  while (GetMessage(&msg, NULL, 0, 0) > 0) {
     TranslateMessage(&msg);
     DispatchMessage(&msg);
   }
-
-  return 0;
 
   return 0;
 }
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-  switch (uMsg)
-  {
-  case WM_DESTROY:
-    PostQuitMessage(0);
-    return 0;
+  switch (uMsg) {
+    case WM_DESTROY: {
+      PostQuitMessage(0);
+    } return 0;
 
-  case WM_PAINT:
-  {
-    PAINTSTRUCT ps;
-    HDC hdc = BeginPaint(hwnd, &ps);
+    case WM_PAINT: {
+      PAINTSTRUCT ps;
+      HDC hdc = BeginPaint(hwnd, &ps);
 
-    // All painting occurs here, between BeginPaint and EndPaint.
-    FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-    EndPaint(hwnd, &ps);
-  }
-    return 0;
+      // All painting occurs here, between BeginPaint and EndPaint.
+      FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+      EndPaint(hwnd, &ps);
+    } return 0;
   }
 
   return DefWindowProcW(hwnd, uMsg, wParam, lParam);
