f384edc9a41e3b7972d5570fc440136f2495cd20
RMvirtual <rmvirtual@AOL.com>
2023-01-13 19:43:50 +0000

    Separate circle header.

--- src/main/windows/demo/direct2D/circle.cpp
+++ src/main/windows/demo/direct2D/circle.cpp
@@ -2,6 +2,7 @@
 #include <d2d1.h>
 #pragma comment(lib, "d2d1")
 
+#include "circle.h"
 #include "windows/demo/gui/oop/base_window.h"
 
 template <class T>
@@ -12,29 +13,6 @@ void SafeRelease(T **ppT)
     *ppT = NULL;
   }
 }
-
-class MainWindow : public BaseWindow<MainWindow>
-{
-  ID2D1Factory *pFactory;
-  ID2D1HwndRenderTarget *pRenderTarget;
-  ID2D1SolidColorBrush *pBrush;
-  D2D1_ELLIPSE ellipse;
-
-  void CalculateLayout();
-  HRESULT CreateGraphicsResources();
-  void DiscardGraphicsResources();
-  void OnPaint();
-  void Resize();
-
-public:
-  MainWindow() : pFactory(NULL), pRenderTarget(NULL), pBrush(NULL)
-  {
-    // Pass.
-  }
-
-  PCWSTR ClassName() const { return L"Circle Window Class"; }
-  LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam);
-};
 
 void MainWindow::CalculateLayout()
 {
@@ -122,7 +100,7 @@ LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
   switch (uMsg) {
     case WM_CREATE:
       if (FAILED(D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &pFactory)))
-        return -1; // Fail CreateWindowEx.
+        return -1;
 
       return 0;
 
