378e0f3bfcb22ee1aaa841831370e5385d8d7838
RMvirtual <rmvirtual@AOL.com>
2023-01-15 17:59:31 +0000

    Made 2022 build tools default standard.

--- script/environment/start_shell.bat
+++ script/environment/start_shell.bat
@@ -1,16 +1,16 @@
 @ECHO OFF
 CALL set_working_directory.bat
 
-IF "%1%"=="" GOTO START_2019
-IF "%1%"=="2019" GOTO START_2019
+IF "%1%"=="" GOTO START_2022
 IF "%1%"=="2022" GOTO START_2022
+IF "%1%"=="2019" GOTO START_2019
 
 :START_2019
-CALL visual_c_2019.bat
+CALL vc_build_tools_2019.bat
 GOTO COMPLETE
 
 :START_2022
-CALL visual_c_2022.bat
+CALL vc_build_tools_2022.bat
 GOTO COMPLETE
 
 :COMPLETE
