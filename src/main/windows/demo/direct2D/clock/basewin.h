38ff778cf9ba70aa7d487b0d8f0eba4d010f735c
RMvirtual <rmvirtual@AOL.com>
2023-01-15 17:54:34 +0000

    Added build tools installer to lib.

--- src/main/windows/demo/direct2D/clock/basewin.h
+++ src/main/windows/demo/direct2D/clock/basewin.h
@@ -1,69 +1,71 @@
-template <class DERIVED_TYPE> 
+#include <windows.h>
+#include <D2d1.h>
+#include <assert.h>
+
+
+template <class DERIVED_TYPE>
 class BaseWindow
 {
 public:
-    static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+  static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+  {
+    DERIVED_TYPE *pThis = NULL;
+
+    if (uMsg == WM_NCCREATE)
     {
-        DERIVED_TYPE *pThis = NULL;
+      CREATESTRUCT *pCreate = (CREATESTRUCT *)lParam;
+      pThis = (DERIVED_TYPE *)pCreate->lpCreateParams;
+      SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)pThis);
 
-        if (uMsg == WM_NCCREATE)
-        {
-            CREATESTRUCT* pCreate = (CREATESTRUCT*)lParam;
-            pThis = (DERIVED_TYPE*)pCreate->lpCreateParams;
-            SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)pThis);
-
-            pThis->m_hwnd = hwnd;
-        }
-        else
-        {
-            pThis = (DERIVED_TYPE*)GetWindowLongPtr(hwnd, GWLP_USERDATA);
-        }
-        if (pThis)
-        {
-            return pThis->HandleMessage(uMsg, wParam, lParam);
-        }
-        else
-        {
-            return DefWindowProc(hwnd, uMsg, wParam, lParam);
-        }
+      pThis->m_hwnd = hwnd;
     }
-
-    BaseWindow() : m_hwnd(NULL) { }
-
-    BOOL Create(
-        PCWSTR lpWindowName,
-        DWORD dwStyle,
-        DWORD dwExStyle = 0,
-        int x = CW_USEDEFAULT,
-        int y = CW_USEDEFAULT,
-        int nWidth = CW_USEDEFAULT,
-        int nHeight = CW_USEDEFAULT,
-        HWND hWndParent = 0,
-        HMENU hMenu = 0
-        )
+    else
     {
-        WNDCLASS wc = {0};
-
-        wc.lpfnWndProc   = DERIVED_TYPE::WindowProc;
-        wc.hInstance     = GetModuleHandle(NULL);
-        wc.lpszClassName = ClassName();
-
-        RegisterClass(&wc);
-
-        m_hwnd = CreateWindowEx(
-            dwExStyle, ClassName(), lpWindowName, dwStyle, x, y,
-            nWidth, nHeight, hWndParent, hMenu, GetModuleHandle(NULL), this
-            );
-
-        return (m_hwnd ? TRUE : FALSE);
+      pThis = (DERIVED_TYPE *)GetWindowLongPtr(hwnd, GWLP_USERDATA);
     }
+    if (pThis)
+    {
+      return pThis->HandleMessage(uMsg, wParam, lParam);
+    }
+    else
+    {
+      return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+  }
 
-    HWND Window() const { return m_hwnd; }
+  BaseWindow() : m_hwnd(NULL) {}
+
+  BOOL Create(
+      PCWSTR lpWindowName,
+      DWORD dwStyle,
+      DWORD dwExStyle = 0,
+      int x = CW_USEDEFAULT,
+      int y = CW_USEDEFAULT,
+      int nWidth = CW_USEDEFAULT,
+      int nHeight = CW_USEDEFAULT,
+      HWND hWndParent = 0,
+      HMENU hMenu = 0)
+  {
+    WNDCLASS wc = {0};
+
+    wc.lpfnWndProc = DERIVED_TYPE::WindowProc;
+    wc.hInstance = GetModuleHandle(NULL);
+    wc.lpszClassName = ClassName();
+
+    RegisterClass(&wc);
+
+    m_hwnd = CreateWindowEx(
+        dwExStyle, ClassName(), lpWindowName, dwStyle, x, y,
+        nWidth, nHeight, hWndParent, hMenu, GetModuleHandle(NULL), this);
+
+    return (m_hwnd ? TRUE : FALSE);
+  }
+
+  HWND Window() const { return m_hwnd; }
 
 protected:
+  virtual PCWSTR ClassName() const = 0;
+  virtual LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam) = 0;
 
-    virtual PCWSTR  ClassName() const = 0;
-    virtual LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam) = 0;
-
-    HWND m_hwnd;
+  HWND m_hwnd;
 };
--- src/main/windows/demo/direct2D/clock/main.cpp
+++ src/main/windows/demo/direct2D/clock/main.cpp
@@ -1,30 +1,29 @@
+#include <atlbase.h>
 #include <windows.h>
 #include <D2d1.h>
 #include <assert.h>
-#include <atlbase.h>
 
 #pragma comment(lib, "d2d1")
 
 #include "basewin.h"
 #include "scene.h"
 
-
 class Scene : public GraphicsScene
 {
     CComPtr<ID2D1SolidColorBrush> m_pFill;
     CComPtr<ID2D1SolidColorBrush> m_pStroke;
 
-    D2D1_ELLIPSE          m_ellipse;
-    D2D_POINT_2F          m_Ticks[24];
+    D2D1_ELLIPSE m_ellipse;
+    D2D_POINT_2F m_Ticks[24];
 
     HRESULT CreateDeviceIndependentResources() { return S_OK; }
-    void    DiscardDeviceIndependentResources() { }
+    void DiscardDeviceIndependentResources() {}
     HRESULT CreateDeviceDependentResources();
-    void    DiscardDeviceDependentResources();
-    void    CalculateLayout();
-    void    RenderScene();
+    void DiscardDeviceDependentResources();
+    void CalculateLayout();
+    void RenderScene();
 
-    void    DrawClockHand(float fHandLength, float fAngle, float fStrokeWidth);
+    void DrawClockHand(float fHandLength, float fAngle, float fStrokeWidth);
 };
 
 HRESULT Scene::CreateDeviceDependentResources()
@@ -32,38 +31,32 @@ HRESULT Scene::CreateDeviceDependentResources()
     HRESULT hr = m_pRenderTarget->CreateSolidColorBrush(
         D2D1::ColorF(1.0f, 1.0f, 0),
         D2D1::BrushProperties(),
-        &m_pFill
-        );
+        &m_pFill);
 
     if (SUCCEEDED(hr))
     {
         hr = m_pRenderTarget->CreateSolidColorBrush(
             D2D1::ColorF(0, 0, 0),
             D2D1::BrushProperties(),
-            &m_pStroke
-            );
+            &m_pStroke);
     }
     return hr;
 }
 
-
 void Scene::DrawClockHand(float fHandLength, float fAngle, float fStrokeWidth)
 {
     m_pRenderTarget->SetTransform(
-        D2D1::Matrix3x2F::Rotation(fAngle, m_ellipse.point)
-            );
+        D2D1::Matrix3x2F::Rotation(fAngle, m_ellipse.point));
 
     // endPoint defines one end of the hand.
     D2D_POINT_2F endPoint = D2D1::Point2F(
         m_ellipse.point.x,
-        m_ellipse.point.y - (m_ellipse.radiusY * fHandLength)
-        );
+        m_ellipse.point.y - (m_ellipse.radiusY * fHandLength));
 
     // Draw a line from the center of the ellipse to endPoint.
     m_pRenderTarget->DrawLine(
         m_ellipse.point, endPoint, m_pStroke, fStrokeWidth);
 }
-
 
 void Scene::RenderScene()
 {
@@ -75,7 +68,7 @@ void Scene::RenderScene()
     // Draw tick marks
     for (DWORD i = 0; i < 12; i++)
     {
-        m_pRenderTarget->DrawLine(m_Ticks[i*2], m_Ticks[i*2+1], m_pStroke, 2.0f);
+        m_pRenderTarget->DrawLine(m_Ticks[i * 2], m_Ticks[i * 2 + 1], m_pStroke, 2.0f);
     }
 
     // Draw hands
@@ -83,18 +76,18 @@ void Scene::RenderScene()
     GetLocalTime(&time);
 
     // 60 minutes = 30 degrees, 1 minute = 0.5 degree
-    const float fHourAngle = (360.0f / 12) * (time.wHour) + (time.wMinute * 0.5f); 
-    const float fMinuteAngle =(360.0f / 60) * (time.wMinute);
+    const float fHourAngle = (360.0f / 12) * (time.wHour) + (time.wMinute * 0.5f);
+    const float fMinuteAngle = (360.0f / 60) * (time.wMinute);
 
-    const float fSecondAngle = 
+    const float fSecondAngle =
         (360.0f / 60) * (time.wSecond) + (360.0f / 60000) * (time.wMilliseconds);
 
-    DrawClockHand(0.6f,  fHourAngle,   6);
+    DrawClockHand(0.6f, fHourAngle, 6);
     DrawClockHand(0.85f, fMinuteAngle, 4);
     DrawClockHand(0.85f, fSecondAngle, 1);
 
     // Restore the identity transformation.
-    m_pRenderTarget->SetTransform( D2D1::Matrix3x2F::Identity() );
+    m_pRenderTarget->SetTransform(D2D1::Matrix3x2F::Identity());
 }
 
 void Scene::CalculateLayout()
@@ -105,30 +98,27 @@ void Scene::CalculateLayout()
     const float y = fSize.height / 2.0f;
     const float radius = min(x, y);
 
-    m_ellipse = D2D1::Ellipse( D2D1::Point2F(x, y), radius, radius);
+    m_ellipse = D2D1::Ellipse(D2D1::Point2F(x, y), radius, radius);
 
     // Calculate tick marks.
 
     D2D_POINT_2F pt1 = D2D1::Point2F(
         m_ellipse.point.x,
-        m_ellipse.point.y - (m_ellipse.radiusY * 0.9f)
-        );
+        m_ellipse.point.y - (m_ellipse.radiusY * 0.9f));
 
     D2D_POINT_2F pt2 = D2D1::Point2F(
         m_ellipse.point.x,
-        m_ellipse.point.y - m_ellipse.radiusY
-        );
+        m_ellipse.point.y - m_ellipse.radiusY);
 
     for (DWORD i = 0; i < 12; i++)
     {
         D2D1::Matrix3x2F mat = D2D1::Matrix3x2F::Rotation(
             (360.0f / 12) * i, m_ellipse.point);
 
-        m_Ticks[i*2] = mat.TransformPoint(pt1);
-        m_Ticks[i*2 + 1] = mat.TransformPoint(pt2);
+        m_Ticks[i * 2] = mat.TransformPoint(pt1);
+        m_Ticks[i * 2 + 1] = mat.TransformPoint(pt2);
     }
 }
-
 
 void Scene::DiscardDeviceDependentResources()
 {
@@ -136,29 +126,26 @@ void Scene::DiscardDeviceDependentResources()
     m_pStroke.Release();
 }
 
-
 class MainWindow : public BaseWindow<MainWindow>
 {
-    HANDLE  m_hTimer;
+    HANDLE m_hTimer;
     Scene m_scene;
 
-    BOOL    InitializeTimer();
+    BOOL InitializeTimer();
 
 public:
-
     MainWindow() : m_hTimer(NULL)
     {
     }
 
-    void    WaitTimer();
+    void WaitTimer();
 
-    PCWSTR  ClassName() const { return L"Clock Window Class"; }
+    PCWSTR ClassName() const { return L"Clock Window Class"; }
     LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam);
 };
 
-// Constants 
+// Constants
 const WCHAR WINDOW_NAME[] = L"Analog Clock";
-
 
 INT WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, INT nCmdShow)
 {
@@ -166,7 +153,7 @@ INT WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, INT nCmdShow)
     {
         return 0;
     }
-    
+
     MainWindow win;
 
     if (!win.Create(WINDOW_NAME, WS_OVERLAPPEDWINDOW))
@@ -178,7 +165,7 @@ INT WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, INT nCmdShow)
 
     // Run the message loop.
 
-    MSG msg = { };
+    MSG msg = {};
     while (msg.message != WM_QUIT)
     {
         if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
@@ -215,21 +202,21 @@ LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
 
     case WM_PAINT:
     case WM_DISPLAYCHANGE:
-        {
-            PAINTSTRUCT ps;
-            BeginPaint(m_hwnd, &ps);
-            m_scene.Render(m_hwnd);
-            EndPaint(m_hwnd, &ps);
-        }
+    {
+        PAINTSTRUCT ps;
+        BeginPaint(m_hwnd, &ps);
+        m_scene.Render(m_hwnd);
+        EndPaint(m_hwnd, &ps);
+    }
         return 0;
 
     case WM_SIZE:
-        {
-            int x = (int)(short)LOWORD(lParam);
-            int y = (int)(short)HIWORD(lParam);
-            m_scene.Resize(x,y);
-            InvalidateRect(m_hwnd, NULL, FALSE);
-        }
+    {
+        int x = (int)(short)LOWORD(lParam);
+        int y = (int)(short)HIWORD(lParam);
+        m_scene.Resize(x, y);
+        InvalidateRect(m_hwnd, NULL, FALSE);
+    }
         return 0;
 
     case WM_ERASEBKGND:
@@ -239,7 +226,6 @@ LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
         return DefWindowProc(hwnd, uMsg, wParam, lParam);
     }
 }
-
 
 BOOL MainWindow::InitializeTimer()
 {
@@ -251,7 +237,7 @@ BOOL MainWindow::InitializeTimer()
 
     LARGE_INTEGER li = {0};
 
-    if (!SetWaitableTimer(m_hTimer, &li, (1000/60), NULL, NULL,FALSE))
+    if (!SetWaitableTimer(m_hTimer, &li, (1000 / 60), NULL, NULL, FALSE))
     {
         CloseHandle(m_hTimer);
         m_hTimer = NULL;
@@ -264,8 +250,7 @@ BOOL MainWindow::InitializeTimer()
 void MainWindow::WaitTimer()
 {
     // Wait until the timer expires or any message is posted.
-    if (MsgWaitForMultipleObjects(1, &m_hTimer, FALSE, INFINITE, QS_ALLINPUT) 
-            == WAIT_OBJECT_0)
+    if (MsgWaitForMultipleObjects(1, &m_hTimer, FALSE, INFINITE, QS_ALLINPUT) == WAIT_OBJECT_0)
     {
         InvalidateRect(m_hwnd, NULL, FALSE);
     }
