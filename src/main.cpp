b7ad140dd81bc0d10347a3e5d207f284f9f22844
RMvirtual <rmvirtual@AOL.com>
2023-08-06 10:15:20 +0100

    Tidy up.

--- src/main.cpp
+++ src/main.cpp
@@ -16,9 +16,9 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         int width = 200;
         int height = 200;
 
-        // Draw the bitmap on the window
+        // Draw bitmap onto window.
         HDC hdcMem = CreateCompatibleDC(hdc);
-        HBITMAP hOldBitmap = (HBITMAP)SelectObject(hdcMem, hBitmap);
+        HBITMAP hOldBitmap = (HBITMAP) SelectObject(hdcMem, hBitmap);
         BitBlt(hdc, 0, 0, width, height, hdcMem, 0, 0, SRCCOPY);
         SelectObject(hdcMem, hOldBitmap);
         DeleteDC(hdcMem);
@@ -26,12 +26,15 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         EndPaint(hWnd, &ps);
         break;
     }
+
     case WM_CLOSE:
         DestroyWindow(hWnd);
         break;
+
     case WM_DESTROY:
         PostQuitMessage(0);
         break;
+
     default:
         return DefWindowProcW(hWnd, message, wParam, lParam);
     }
@@ -39,22 +42,25 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     return 0;
 }
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
-{
-    hInst = hInstance;
 
-    // Create an in-memory device context and compatible bitmap.
+int WINAPI WinMain(
+    HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine,
+    int nCmdShow
+) {
+    hInst = hInstance;
+    int width = 400;  
+    int height = 300;
+
+    // Create in-memory device context and compatible bitmap.
     HDC hdc = GetDC(NULL);
     HDC hdcMem = CreateCompatibleDC(hdc);
     
-    int width = 400;  
-    int height = 300;
     hBitmap = CreateCompatibleBitmap(hdc, width, height);
     SelectObject(hdcMem, hBitmap);
 
     // Draw on in-memory bitmap.
     HBRUSH hBrush = CreateSolidBrush(RGB(255, 0, 0));
-    RECT rect = { 0, 0, width, height };
+    RECT rect {0, 0, width, height};
     FillRect(hdcMem, &rect, hBrush);
     DeleteObject(hBrush);
 
@@ -62,9 +68,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     DeleteDC(hdcMem);
     ReleaseDC(NULL, hdc);    
 
-    // Amended copy-paste ends here.
     // Register window class.
-    WNDCLASSEXW wcex = {
+    WNDCLASSEXW wcex {
         sizeof(WNDCLASSEXW), CS_HREDRAW | CS_VREDRAW, WndProc, 0L, 0L,
         GetModuleHandle(NULL), NULL, NULL, NULL, NULL, L"BitmapWindowClass",
         NULL
