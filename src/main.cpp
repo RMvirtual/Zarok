0aaae9ce680e71ce301b81a7a209af1d75c19dc9
RMvirtual <rmvirtual@AOL.com>
2023-08-06 10:44:06 +0100

    Slide functions.

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,68 @@ HWND WINDOW_HANDLER;
 HBITMAP BITMAP_BUFFER;
 
 
-LRESULT CALLBACK WndProc(
+LRESULT CALLBACK processEvent(
+    HWND windowHandle, UINT message, WPARAM wParam, LPARAM lParam);
+
+
+int WINAPI WinMain(
+    HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine,
+    int nCmdShow
+) {
+    WINDOWS_INSTANCE = hInstance;
+    int width = 400;  
+    int height = 300;
+
+    // Create in-memory device context and compatible bitmap.
+    HDC deviceContext = GetDC(NULL);
+    HDC deviceContextBuffer = CreateCompatibleDC(deviceContext);
+    
+    BITMAP_BUFFER = CreateCompatibleBitmap(deviceContext, width, height);
+    SelectObject(deviceContextBuffer, BITMAP_BUFFER);
+
+    // Draw on in-memory bitmap.
+    HBRUSH brush = CreateSolidBrush(RGB(255, 0, 0));
+    RECT rectangle {0, 0, width, height};
+    FillRect(deviceContextBuffer, &rectangle, brush);
+    DeleteObject(brush);
+
+    // Release resources.
+    DeleteDC(deviceContextBuffer);
+    ReleaseDC(NULL, deviceContext);    
+
+    // Register window class.
+    WNDCLASSEXW windowClass {
+        sizeof(WNDCLASSEXW), CS_HREDRAW | CS_VREDRAW, processEvent, 0L, 0L,
+        GetModuleHandle(NULL), NULL, NULL, NULL, NULL, L"BitmapWindowClass",
+        NULL
+    };
+    
+    RegisterClassExW(&windowClass);
+
+    // Create window.
+    WINDOW_HANDLER = CreateWindowW(
+        L"BitmapWindowClass", L"Bitmap Window",
+        WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
+        width, height, NULL, NULL, hInstance, NULL
+    );
+
+    // Display window.
+    ShowWindow(WINDOW_HANDLER, nCmdShow);
+    UpdateWindow(WINDOW_HANDLER);
+
+    // Event loop.
+    MSG event;
+
+    while (GetMessageW(&event, NULL, 0, 0)) {
+        TranslateMessage(&event);
+        DispatchMessageW(&event);
+    }
+
+    return (int) event.wParam;
+}
+
+
+LRESULT CALLBACK processEvent(
     HWND windowHandle, UINT message, WPARAM wParam, LPARAM lParam)
 {
     switch (message) {
@@ -41,61 +102,4 @@ LRESULT CALLBACK WndProc(
     }
     
     return 0;
-}
-
-
-int WINAPI WinMain(
-    HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine,
-    int nCmdShow
-) {
-    WINDOWS_INSTANCE = hInstance;
-    int width = 400;  
-    int height = 300;
-
-    // Create in-memory device context and compatible bitmap.
-    HDC deviceContext = GetDC(NULL);
-    HDC deviceContextBuffer = CreateCompatibleDC(deviceContext);
-    
-    BITMAP_BUFFER = CreateCompatibleBitmap(deviceContext, width, height);
-    SelectObject(deviceContextBuffer, BITMAP_BUFFER);
-
-    // Draw on in-memory bitmap.
-    HBRUSH brush = CreateSolidBrush(RGB(255, 0, 0));
-    RECT rectangle {0, 0, width, height};
-    FillRect(deviceContextBuffer, &rectangle, brush);
-    DeleteObject(brush);
-
-    // Release resources.
-    DeleteDC(deviceContextBuffer);
-    ReleaseDC(NULL, deviceContext);    
-
-    // Register window class.
-    WNDCLASSEXW windowClass {
-        sizeof(WNDCLASSEXW), CS_HREDRAW | CS_VREDRAW, WndProc, 0L, 0L,
-        GetModuleHandle(NULL), NULL, NULL, NULL, NULL, L"BitmapWindowClass",
-        NULL
-    };
-    
-    RegisterClassExW(&windowClass);
-
-    // Create window.
-    WINDOW_HANDLER = CreateWindowW(
-        L"BitmapWindowClass", L"Bitmap Window",
-        WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
-        width, height, NULL, NULL, hInstance, NULL
-    );
-
-    // Display window.
-    ShowWindow(WINDOW_HANDLER, nCmdShow);
-    UpdateWindow(WINDOW_HANDLER);
-
-    // Event loop.
-    MSG event;
-
-    while (GetMessageW(&event, NULL, 0, 0)) {
-        TranslateMessage(&event);
-        DispatchMessageW(&event);
-    }
-
-    return (int) event.wParam;
 }
