6f57fe1404b6f910bff2dbc6811c85246b528c96
RMvirtual <rmvirtual@AOL.com>
2023-08-08 21:30:01 +0100

    Added test example.

--- tests/test_example.cpp
+++ tests/test_example.cpp
@@ -1,6 +1,16 @@
+#include <string>
 #include "gtest/gtest.h"
 
 // Your test cases go here
+
+TEST(HelloWorldTest, ShouldPass)
+{
+    std::string result {"Hello, World!"};
+    std::string correctResult {"Hello, World!"};
+
+    EXPECT_STREQ(result.c_str(), correctResult.c_str());
+}
+
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
